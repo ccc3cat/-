630d43672cdedc7ccbe5eb6232022e2a1af42131
ccc3cat <ccc3cat.lego@gmail.com>
2024-05-10 13:03:19 +0800

    5/10

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// 漢堡 
+const openMenuButton = document.getElementById('openMenu');
+const closeMenuButton = document.getElementById('closeMenu');
+const menu = document.getElementById('menu');
+const overlay = document.getElementById('overlay');
+
+openMenuButton.addEventListener('click', () => {
+    menu.classList.add('show-menu');
+    overlay.classList.add('show-overlay');
+});
+
+closeMenuButton.addEventListener('click', () => {
+    menu.classList.remove('show-menu');
+    overlay.classList.remove('show-overlay');
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('show-menu');
+    overlay.classList.remove('show-overlay');
+});
+// TOP的滾動隱藏效果
+window.addEventListener('scroll', function () {
+    var element = document.getElementById('elementToDisappear');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition <= 250) {
+        element.style.display = 'none'; // 滾動到 200px 高度時隱藏元素
+    } else {
+        element.style.display = 'block'; // 如果滾動高度小於 200px，則顯示元素
+    }
+});
+
