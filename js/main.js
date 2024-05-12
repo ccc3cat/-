31b91f25471cb4d323cbc79f50d1b7617ab4ab2c
ccc3cat <ccc3cat.lego@gmail.com>
2024-05-12 19:01:50 +0800

    5/12

--- js/main.js
+++ js/main.js
@@ -17,14 +17,35 @@ closeMenuButton.addEventListener('click', () => {
 
 
 // TOP的滾動隱藏效果
-window.addEventListener('scroll', function () {
+document.addEventListener('DOMContentLoaded', function () {
     var element = document.getElementById('elementToDisappear');
     var scrollPosition = window.scrollY;
 
     if (scrollPosition <= 250) {
-        element.style.display = 'none'; // 滾動到 200px 高度時隱藏元素
+        element.style.display = 'none'; // 滾動到 250px 高度時隱藏元素
     } else {
-        element.style.display = 'block'; // 如果滾動高度小於 200px，則顯示元素
+        element.style.display = 'block'; // 如果滾動高度小於 250px，則顯示元素
     }
+
+    window.addEventListener('scroll', function () {
+        var scrollPosition = window.scrollY;
+
+        if (scrollPosition <= 250) {
+            element.style.display = 'none'; // 滾動到 250px 高度時隱藏元素
+        } else {
+            element.style.display = 'block'; // 如果滾動高度小於 250px，則顯示元素
+        }
+    });
 });
 
+
+$(function () {
+    $("#CarouselImg").cycle({
+        // 輪播效果名稱
+        fx: 'fade',
+        // 第一次輪播時的第一張圖片停留時
+        delay: -4000,
+        // 輪播速度
+        timeout: 500,
+    });
+});
