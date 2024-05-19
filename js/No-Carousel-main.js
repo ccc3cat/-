088d9e52c4dbf846c7e9b8c05f72d770cad8debe
ccc3cat <ccc3cat.lego@gmail.com>
2024-05-19 14:31:13 +0800

    5/19

---
+++ js/No-Carousel-main.js
@@ -0,0 +1,14 @@
+    // TOP至頂按鈕
+    $('#gotop').click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 1300);
+    })
+
+    // 淡出淡入
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            $('#gotop').stop().fadeTo('', 1);
+        }
+        else {
+            $('#gotop').stop().fadeOut();
+        }
+    })
